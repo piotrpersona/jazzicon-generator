9beadb419c5b3626027987e08152fc80d570f791
Piotr Persona <persona.piotr@gmail.com>
2022-08-23 00:01:46 +0200

    change branch to master
    Signed-off-by: Piotr Persona <persona.piotr@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:piotrpersona/jazzicon-generator.git main:gh-pages
+git push -f git@github.com:piotrpersona/jazzicon-generator.git master:gh-pages
 cd -
